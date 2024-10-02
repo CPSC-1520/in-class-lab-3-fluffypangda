0be029ffe1befd2e503d71553f2872da7ea7f5a4
fluffypangda <ztian5@studentmail.nait.ca>
2024-10-01 21:35:19 -0600

    ansewer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // Enter your code below.
+const resourcesDiv = document.querySelector('.javascript-resources'); 
+const showButton = document.getElementById('show-resources'); 
+showButton.addEventListener('click', () => {
+resourcesDiv.classList.remove('d-none');
+});
+resourcesDiv.addEventListener('mouseover', (event) => {
+    console.log(event.target); 
+    event.target.classList.add('fw-bold'); 
+});
+resourcesDiv.addEventListener('mouseout', (event) => {
+    event.target.classList.remove('fw-bold'); 
+});
+resourcesDiv.addEventListener('click', (event) => {
+    event.target.classList.add('fst-italic'); 
+});
